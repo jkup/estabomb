2e9b291dfca48d2fd16ae431b009e7534a0d2f65
Jacob Mather <jmather@mashery.com>
2013-11-10 00:23:04 -0500

    fixed how estimatation is wired in

--- web/js/application.js
+++ web/js/application.js
@@ -1,3 +1,21 @@
 window.Estabomb = Ember.Application.create();
 
 Estabomb.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+
+Estabomb.RoomController = Ember.ArrayController.extend({
+    // initial value
+    hasEstimated: false,
+    actions: {
+        estimate: function(estimate) {
+            console.log(estimate);
+            $('.options').css('display', 'none');
+            this.set('hasEstimated', true);
+        },
+
+        reset: function() {
+            $('.options').css('display', 'block');
+            this.set('hasEstimated', false);
+        }
+    }
+});
