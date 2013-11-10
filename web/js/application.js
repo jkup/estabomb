c7da0a3f1c0c13dfa9b45b820af9919024f18457
Jacob Mather <jmather@mashery.com>
2013-11-10 18:56:32 -0500

    One last bug, hah.

--- web/js/application.js
+++ web/js/application.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://'+window.location.hostname+':5001');
+var socket = io.connect('http://'+window.location.hostname);
 
 window.Estabomb = Ember.Application.create({
 //    LOG_TRANSITIONS: true
