8d46e3bb6fc8239458f0c0072dd7caf2c3d0adb5
Jacob Mather <jmather@mashery.com>
2013-11-10 03:55:08 -0500

    all example tests in the ui frontent are done.

--- web/js/application.js
+++ web/js/application.js
@@ -1,6 +1,8 @@
-window.Estabomb = Ember.Application.create();
+window.Estabomb = Ember.Application.create({
+//    LOG_TRANSITIONS: true
+});
 
-Estabomb.ApplicationAdapter = DS.FixtureAdapter.extend();
+Estabomb.ApplicationAdapter = DS.FixtureAdapter;
 
 Estabomb.LoginController = Ember.Controller.extend({
     actions: {
@@ -37,7 +39,33 @@ Estabomb.RoomController = Ember.ArrayController.extend({
             $('#estimationPanel').modal('show');
         },
 
-        randomEstimateComesIn: function() {
+        mockEstimate: function() {
+            var players = this.get('model')
+            var player = players.findBy('hasEstimated', false);
+            if (player) {
+                this.store.update('player', {
+                    id: player.get('id'),
+                    hasEstimated: true
+                });
+            }
+        },
+
+        mockAllEstimatesSubmitted: function() {
+            var estimates = ['S', 'M', 'L', 'XL'];
+
+            var players = this.get('model');
+
+            var store = this.store;
+
+            players.forEach(function(player) {
+                var estimate_idx = Math.floor(Math.random() * (players.toArray().length + 1));
+
+                store.update('player', {
+                    id: player.get('id'),
+                    hasEstimated: false,
+                    estimate: estimates[estimate_idx]
+                });
+            });
         }
     }
 });
