985378ff3687fae7c8a535cb7e146c7683e685b8
Jacob Mather <jmather@mashery.com>
2013-11-10 00:37:54 -0500

    Made the join room button work 'kind of' like it actually will.

--- web/js/application.js
+++ web/js/application.js
@@ -2,6 +2,16 @@ window.Estabomb = Ember.Application.create();
 
 Estabomb.ApplicationAdapter = DS.FixtureAdapter.extend();
 
+Estabomb.LoginController = Ember.Controller.extend({
+    actions: {
+        joinRoom: function() {
+            var name = $('#name').val();
+            var room = $('#room').val();
+            console.log('Logging in...', [name, room]);
+            this.transitionToRoute('room', 1);
+        }
+    }
+});
 
 Estabomb.RoomController = Ember.ArrayController.extend({
     // initial value
